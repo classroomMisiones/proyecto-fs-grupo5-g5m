08318f60b289b979d36496a06341701e3050edc4
Miguel_S <sotelomiguel.pdas@gmail.com>
2021-07-25 19:27:02 -0300

    routing pagina inicio navbar y footer

--- PILMoney/src/app/app-routing.module.ts
+++ PILMoney/src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ErrorComponent} from './error/error.component';
 
+import { BodyComponent } from './main/body/body.component';
 
 const routes: Routes = [
-  
+  {
+    path:'iniciobody',
+    component:BodyComponent,
+    pathMatch:'full'
+  }
 ];
 
 @NgModule({

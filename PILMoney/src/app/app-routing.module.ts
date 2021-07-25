0aa8a5b52cfa29146f5d16fe0f18d85779fd9404
Gerardo Aguilar <gerardo.aguilar.b@gmail.com>
2021-07-25 18:17:34 -0300

    agrego  el router

--- PILMoney/src/app/app-routing.module.ts
+++ PILMoney/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ErrorComponent} from './error/error.component';
-
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  
+  { path: "login", component: LoginComponent, pathMatch: "full" }
 ];
 
 @NgModule({

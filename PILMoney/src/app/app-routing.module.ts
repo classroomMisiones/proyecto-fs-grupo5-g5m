601c6887dd1d4530e0d5e9b52f60f847c4bc704d
Miguel_S <sotelomiguel.pdas@gmail.com>
2021-07-26 09:34:48 -0300

    ruteo en navbar y footer

--- PILMoney/src/app/app-routing.module.ts
+++ PILMoney/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ErrorComponent} from './error/error.component';
+
+import { BodyComponent } from './main/body/body.component';
 import { LoginComponent } from './login/login.component';
+import { ErrorComponent} from './error/error.component';
+import { RegistroUsuarioComponent } from './registro-usuario/registro-usuario.component'
 
 const routes: Routes = [
-  { path: "login", component: LoginComponent, pathMatch: "full" }
+  
+  { path: 'iniciobody', component: BodyComponent, pathMatch:'full' },
+  { path: "login", component: LoginComponent, pathMatch: "full" },
+  { path: "registro", component: RegistroUsuarioComponent, pathMatch: "full" },
+  { path:'**', component:ErrorComponent }
+
 ];
 
 @NgModule({

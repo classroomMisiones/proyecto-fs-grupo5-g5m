dae7055125c2f8e6c1e06af46f31f628bca95dea
agustinppc <agustinppc@gmail.com>
2021-08-17 11:27:45 -0300

    Creación de página "Nosotros".

--- PILMoney/src/app/app-routing.module.ts
+++ PILMoney/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { BodyComponent } from './main/body/body.component';
 import { LoginComponent } from './login/login.component';
 import { ErrorComponent} from './error/error.component';
-import { RegistroUsuarioComponent } from './registro-usuario/registro-usuario.component'
+import { RegistroUsuarioComponent } from './registro-usuario/registro-usuario.component';
+import { NosotrosComponent } from './nosotros/nosotros.component'
 
 const routes: Routes = [
 
   { path: '', component: BodyComponent, pathMatch:'full' },
   { path: "login", component: LoginComponent, pathMatch: "full" },
   { path: "registro", component: RegistroUsuarioComponent, pathMatch: "full" },
+  { path:"nosotros", component: NosotrosComponent, pathMatch: 'full' }, 
   { path:'**', component:ErrorComponent }
 
 ];

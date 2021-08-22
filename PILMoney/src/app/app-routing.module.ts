bbbf9a271ede183839212e8bd1a3cec0a1ddc23f
Gerardo Aguilar <gerardo.aguilar.b@gmail.com>
2021-08-22 11:57:37 -0300

    vinculando login con dashboard

--- PILMoney/src/app/app-routing.module.ts
+++ PILMoney/src/app/app-routing.module.ts
@@ -5,14 +5,15 @@ import { BodyComponent } from './main/body/body.component';
 import { LoginComponent } from './login/login.component';
 import { ErrorComponent} from './error/error.component';
 import { RegistroUsuarioComponent } from './registro-usuario/registro-usuario.component';
-import { NosotrosComponent } from './nosotros/nosotros.component'
-
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 const routes: Routes = [
 
   { path: '', component: BodyComponent, pathMatch:'full' },
   { path: "login", component: LoginComponent, pathMatch: "full" },
   { path: "registro", component: RegistroUsuarioComponent, pathMatch: "full" },
   { path:"nosotros", component: NosotrosComponent, pathMatch: 'full' }, 
+  { path:"dashboard", component: DashboardComponent, pathMatch: 'full' },
   { path:'**', component:ErrorComponent }
 
 ];

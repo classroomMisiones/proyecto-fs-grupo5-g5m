3f2ac5a5cbaa358ccaa96e9d9a4474fae1b38211
Rosa Rivas <rivas-rossy@hotmail.com.ar>
2021-09-13 17:39:42 -0300

    Pantalla configuracion de cuenta

--- PILMoney/src/app/app-routing.module.ts
+++ PILMoney/src/app/app-routing.module.ts
@@ -7,13 +7,16 @@ import { ErrorComponent} from './error/error.component';
 import { RegistroUsuarioComponent } from './registro-usuario/registro-usuario.component';
 import { NosotrosComponent } from './nosotros/nosotros.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ConfiguracionComponent } from './configuracion/configuracion.component';
+
 const routes: Routes = [
 
   { path: '', component: BodyComponent, pathMatch:'full' },
   { path: "login", component: LoginComponent, pathMatch: "full" },
   { path: "registro", component: RegistroUsuarioComponent, pathMatch: "full" },
-  { path:"nosotros", component: NosotrosComponent, pathMatch: 'full' }, 
+  { path:"nosotros", component: NosotrosComponent, pathMatch: 'full' },
   { path:"dashboard", component: DashboardComponent, pathMatch: 'full' },
+  { path:"configuracion", component: ConfiguracionComponent, pathMatch: 'full' },
   { path:'**', component:ErrorComponent }
 
 ];

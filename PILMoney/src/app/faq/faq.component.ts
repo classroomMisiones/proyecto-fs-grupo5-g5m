b2b376fd4a080641c0b25c7e689582e69c51cbd8
agustinppc <agustinppc@gmail.com>
2021-09-14 14:16:29 -0300

    Actualización FAQ

--- PILMoney/src/app/faq/faq.component.ts
+++ PILMoney/src/app/faq/faq.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-faq',
+  selector: 'app-error',
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.css']
 })

50446c753e57ceb5ca40aea915f5d9f67a785aef
agustinppc <agustinppc@gmail.com>
2021-10-18 16:26:16 -0300

    Corrección FAQ.ts

--- PILMoney/src/app/faq/faq.component.ts
+++ PILMoney/src/app/faq/faq.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-error',
+  selector: 'app-faq',
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.css']
 })
